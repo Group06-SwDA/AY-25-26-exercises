135c1e44fc09426e50edef4728eccd5faff7374a
SteAlve <s354116@studenti.polito.it>
2026-03-19 15:20:50 +0100

    fix: fixed IVehicle in LSP

--- Ex-1-SOLID/src/main/java/solid/lsp/compliant/IGroundVehicle.java
+++ Ex-1-SOLID/src/main/java/solid/lsp/compliant/IGroundVehicle.java
@@ -1,7 +1,5 @@
 package solid.lsp.compliant;
 
-import solid.lsp.violation.IVechicle;
-
 public interface IGroundVehicle extends IVechicle{
     public void switchPassengerAirBag(boolean on);
 }
--- Ex-1-SOLID/src/main/java/solid/lsp/compliant/IVechicle.java
+++ Ex-1-SOLID/src/main/java/solid/lsp/compliant/IVechicle.java
@@ -1,7 +1,6 @@
 package solid.lsp.compliant;
 
 public interface IVechicle {
-
 	public int changeGear(int n);
 	public void accelerate();
 }
